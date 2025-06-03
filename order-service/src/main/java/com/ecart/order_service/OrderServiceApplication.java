4d03af70e4062dbb504c2a71415612f1579849de
sivashanker <sivashanker6353005@gmail.com>
2025-06-03 10:45:58 +0530

    added discovery server to parent pom and added discovery clients to child microservices

--- order-service/src/main/java/com/ecart/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/ecart/order_service/OrderServiceApplication.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @EnableEurekaClient // Uncomment if you want to register this service with Eureka
+
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
